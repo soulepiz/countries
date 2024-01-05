97cc94044cc9ab537ad6c63c3d31e2446407c489
soulepiz <abhinavtp05@gmail.com>
2024-01-05 13:35:06 +0530

    initial

---
+++ pattern.js
@@ -0,0 +1,21 @@
+
+for(row=5;row>=1;row--){
+    str=``
+ for(spc=10;spc>row;spc--){
+        str=str+' '
+    }
+for(col=1;col<=row;col++){
+    str=str+`* `
+}
+    console.log(str);
+}
+for(row=1;row<=5;row++){
+    str=``
+ for(spc=10;spc>row;spc--){
+        str=str+' '
+    }
+for(col=1;col<=row;col++){
+    str=str+`* `
+}
+    console.log(str);
+}
